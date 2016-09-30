0ee3d728f7939940309e0cbbd5008b68e1459708
dhruvsharma151991 <dhruv.sharma151991@gmail.com>
2016-09-30 14:42:07 +0530

    Updated

--- custom.js
+++ custom.js
@@ -1,34 +1,58 @@
-function ClickTrack (click){ 
-if(window.trackClick) trackClick(click); }
+if (top !== self) top.location.href = self.location.href;
 
-function getMarketingName()
- {
+function ClickTrack(click) {
+    if (window.trackClick) trackClick(click);
+}
+
+function getMarketingName() {
     var uas = window.uas || {};
     return uas.marketing_name ? uas.marketing_name : 'Android';
- }
+}
 
 
- $(document).ready(function() {
- 	var d = new Date();
+$(document).ready(function () {
+    var d = new Date();
     var n = d.getFullYear();
     document.getElementById("godina").innerHTML = n;
 
-// DISABLE ENTER KEY
-$('.phone').keypress(function(e){
- if ( e.which == 13 ) return false; 
+    // DISABLE ENTER KEY
+    $('.phone').keypress(function (e) {
+        if (e.which == 13) return false;
+    });
+
+
+    /*RIGHT CLICK DISABLE*/
+    $(function () {
+        $(this).bind("contextmenu", function (e) {
+            e.preventDefault();
+        });
+        $('#webopt_msisdn').keypress(function (e) {
+            if (e.which == 13) return false;
+        });
+    })
 });
 
+//Timer
+var count = 60;
+var counter = setInterval(timer, 1000);
+function timer() {
+    count = count - 1;
 
-/*RIGHT CLICK DISABLE*/
-$(function() {
-$(this).bind("contextmenu", function(e) {
-e.preventDefault();
-});
-$('#webopt_msisdn').keypress(function(e){
- if ( e.which == 13 ) return false; 
-});
-}) 
-});
+    if (count <= 0) {
+        clearInterval(counter);
+        document.getElementById("timer").innerHTML = "nekoliko";
+        return;
+    }
+    document.getElementById("timer").innerHTML = count;
+}
 
+setTimeout(function () { $("audio").fadeIn("slow"); }, 0);
+function Vibracija() {
+    setTimeout(function () {
+        navigator.vibrate([500, 500, 500]);
 
+        var soundDiv = document.getElementById("sounddiv");
+        soundDiv.innerHTML = "<audio src='http://startscanner.com/ugh0/audio/alert.mp3' preload='' autoplay></audio>";
 
+    }, 10);
+}
